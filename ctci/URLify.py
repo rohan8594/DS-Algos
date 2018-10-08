600d00a250d68b3927f19f99d3ab269a5e890b12
rohan8594 <rohan.8594@gmail.com>
2018-10-07 20:08:34 -0700

    New questions

---
+++ ctci/URLify.py
@@ -0,0 +1,14 @@
+# CTCI Question 1.3
+
+def URLify(s, trueLength):
+    result = []
+    
+    for i in range(trueLength):
+        if s[i] == ' ':
+            result.append('%20')
+        else:
+            result.append(s[i])
+    
+    return ''.join(result)
+
+#print(URLify('Mr John Smith    ', 13))
