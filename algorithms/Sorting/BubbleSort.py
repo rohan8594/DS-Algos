d9a4ba9d2844f1a1f1de3ae531ee294d54c83673
rohan8594 <rohan.8594@gmail.com>
2019-02-19 08:16:01 -0800

    Sorting

--- algorithms/Sorting/BubbleSort.py
+++ algorithms/Sorting/BubbleSort.py
@@ -1,13 +1,12 @@
 def bubbleSort(arr):
 
-	for i in range(len(arr) - 1, 0, -1):
+    for i in range(len(arr) - 1, 0, -1):
 
-		for k in range(i):
-			if arr[k] > arr[k + 1]:
-				temp = arr[k]
-				arr[k] = arr[k + 1]
-				arr[k + 1] = temp
+        for k in range(i):
+            if arr[k] > arr[k + 1]:
+                arr[k], arr[k + 1] = arr[k + 1], arr[k]
 
-	return arr
+    return arr
 
-print(bubbleSort([1,6,4,5,2,1,7,3]))
+
+print(bubbleSort([1, 6, 4, 5, 2, 1, 7, 3]))
