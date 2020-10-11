13abc09f8ffe817681705e515b2e5c6188ebbce7
Shubham Goswami <cool.shubham2704@gmail.com>
2020-10-11 14:31:29 +0530

    fixed the issue

--- algorithms/Sorting/InsertionSort.py
+++ algorithms/Sorting/InsertionSort.py
@@ -6,8 +6,8 @@ def insertionSort(arr):
       j = i-1
       while j >=0 and key < arr[j] :
          arr[j+1] = arr[j]
-         j -= 1          
-       arr[j+1] = key
+         j -= 1
+      arr[j+1] = key
 # main
 arr = ['t','u','t','o','r','i','a','l']
 insertionSort(arr)
